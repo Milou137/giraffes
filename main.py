d7cefede1ae832950acca047cd68f68b6add729c
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-14 00:25:28 +0200

    Add Giraffe class

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+DEFAULT_NECK_LENGTH = 100
+
+class Giraffe():
+
+    def __init__(self, neck_length = DEFAULT_NECK_LENGTH):
+
+        self.neck_length = neck_length
 def main():
+    giraffes = []
+    for i in range(3):
+        giraffes.append(Giraffe())
 main()
