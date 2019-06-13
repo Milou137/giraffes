73cdae90c5f374bb670f6f06b4ba8bdbc2fa1eab
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-14 00:31:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ class Giraffe():
                 return True
         else:
             return False
+    def drain_hunger(self):
+        self.hunger -= 1
+        if self.hunger == 0:
+            print("a giraffe died with necklength",self.neck_length)
+            self.dead = True
     def isDead(self):
         return self.dead
 def main():
@@ -26,7 +31,14 @@ def main():
         giraffes.append(Giraffe())
     Game = True
     generation = 0
+    tree_length = 100
+    print("GAME STARTS!","there are", len(giraffes))
     while Game:
+        for hunger_drain in range(5):
+            for g_index in range(len(giraffes)):
+                giraffe = giraffes[g_index]
+                giraffe.drain_hunger()
+                giraffe.eat(tree_length)
         input("press enter to go to the next generation")
         print("there are",len(giraffes),"in generation",generation)
         generation += 1
