c2006ccea35462b6beacbe09c0ecedc88aa08750
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-14 00:27:04 +0200

    add eat method
    and some global constants

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+EAT_RANGE = 10
+MAX_HUNGER = 5
 DEFAULT_NECK_LENGTH = 100
 
 class Giraffe():
@@ -5,6 +7,16 @@ class Giraffe():
     def __init__(self, neck_length = DEFAULT_NECK_LENGTH):
 
         self.neck_length = neck_length
+        self.hunger = MAX_HUNGER
+    def eat(self, tree_length):
+        if ((self.neck_length - tree_length) > -EAT_RANGE) and (
+            (self.neck_length - tree_length) < EAT_RANGE):
+
+            if self.hunger < MAX_HUNGER+1:
+                self.hunger += 1
+                return True
+        else:
+            return False
 def main():
     giraffes = []
     for i in range(3):
