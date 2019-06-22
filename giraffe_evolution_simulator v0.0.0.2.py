2c7590272c6bea9cd24e54fb7393312709139241
Milain Lambers <32010930+Queuebee2@users.noreply.github.com>
2019-06-22 23:23:22 +0200

    add lots of cool

--- giraffe_evolution_simulator v0.0.0.2.py
+++ giraffe_evolution_simulator v0.0.0.2.py
@@ -4,7 +4,6 @@
 
 
 
-# imports
 # ██╗███╗   ███╗██████╗  ██████╗ ██████╗ ████████╗███████╗
 # ██║████╗ ████║██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝
 # ██║██╔████╔██║██████╔╝██║   ██║██████╔╝   ██║   ███████╗
@@ -73,6 +72,18 @@ DEFAULT_GIRAFFE_START_AMOUNT = 25
 DEFAULT_MAX_ALLOWED_GIRAFFES = 30
 DEFAULT_CHANCE_SPAWN_NEW_GIRAFFE = 25 # 1/33
 DEFAULT_MUTATION_AMOUNT = 5
+
+
+
+"""
+ ██████╗ ██╗██████╗  █████╗ ███████╗███████╗███████╗
+██╔════╝ ██║██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+██║  ███╗██║██████╔╝███████║█████╗  █████╗  █████╗  
+██║   ██║██║██╔══██╗██╔══██║██╔══╝  ██╔══╝  ██╔══╝  
+╚██████╔╝██║██║  ██║██║  ██║██║     ██║     ███████╗
+ ╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚══════╝"""
+                                                    
+
 
 
 class Giraffe():
@@ -145,6 +156,15 @@ class Giraffe():
             if self.y > y:
                 self.y = (y/2)
 
+                self.y = y/2
+
+"""
+████████╗██████╗ ███████╗███████╗
+╚══██╔══╝██╔══██╗██╔════╝██╔════╝
+   ██║   ██████╔╝█████╗  █████╗  
+   ██║   ██╔══██╗██╔══╝  ██╔══╝  
+   ██║   ██║  ██║███████╗███████╗
+   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝"""                              
 class Boom():
     def __init__(self, offset=0):
         self.image = pygame.image.load('images/boom_0.png')
@@ -234,6 +254,48 @@ def highest_frequency(l):
     return "Most occuring item was %s with a frequency of %s " %(item,highest)
         
 
+"""
+██████╗ ██╗   ██╗████████╗████████╗ ██████╗ ███╗   ██╗
+██╔══██╗██║   ██║╚══██╔══╝╚══██╔══╝██╔═══██╗████╗  ██║
+██████╔╝██║   ██║   ██║      ██║   ██║   ██║██╔██╗ ██║
+██╔══██╗██║   ██║   ██║      ██║   ██║   ██║██║╚██╗██║
+██████╔╝╚██████╔╝   ██║      ██║   ╚██████╔╝██║ ╚████║
+╚═════╝  ╚═════╝    ╚═╝      ╚═╝    ╚═════╝ ╚═╝  ╚═══╝
+                                                      """
+class button():
+    def __init__(self, color, x, y, width, height, text=''):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+
+    def draw(self, win, outline=None):
+        # Call this method to draw the button on the screen
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height), 0)
+
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 60)
+            text = font.render(self.text, 1, (0, 0, 0))
+            win.blit(text, (
+            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def isOver(self, pos):
+        # Pos is the mouse position or a tuple of (x,y) coordinates
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+
+        return False
+
+upButton = button((0,255,0), 70, 20,50, 50,  '+')
+downButton = button((255,0,0), 150, 20,50, 50,  '-')
+buttons = [upButton, downButton]       
+
 TREEDOWN = - 3
 TREEUP = 15
 
@@ -270,6 +332,16 @@ tree_length = DEFAULT_TREE_LENGTH # DO THIS FOR EVERYTHING
 bob = Boom(420)
 job = Boom(-350)
 
+
+
+#  ██████╗  █████╗ ███╗   ███╗███████╗    ██╗      ██████╗  ██████╗ ██████╗ 
+# ██╔════╝ ██╔══██╗████╗ ████║██╔════╝    ██║     ██╔═══██╗██╔═══██╗██╔══██╗
+# ██║  ███╗███████║██╔████╔██║█████╗      ██║     ██║   ██║██║   ██║██████╔╝
+# ██║   ██║██╔══██║██║╚██╔╝██║██╔══╝      ██║     ██║   ██║██║   ██║██╔═══╝ 
+# ╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗    ███████╗╚██████╔╝╚██████╔╝██║     
+#  ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝    ╚══════╝ ╚═════╝  ╚═════╝ ╚═╝     
+
+                                                                           
 print("GAME STARTS!","there are", len(giraffes))
 while len(giraffes) > 0:
     for event in pygame.event.get():
@@ -364,6 +436,15 @@ while len(giraffes) > 0:
 
 # end state ( diagrams, popuptext, idfk)
 surface.blit(background,(0,0))
+
+'███████╗███╗   ██╗██████╗  ' #  ███████╗ ██████╗██████╗ ███████╗███████╗███╗   ██╗
+'██╔════╝████╗  ██║██╔══██╗ ' #  ██╔════╝██╔════╝██╔══██╗██╔════╝██╔════╝████╗  ██║
+'█████╗  ██╔██╗ ██║██║  ██║ ' #  ███████╗██║     ██████╔╝█████╗  █████╗  ██╔██╗ ██║
+'██╔══╝  ██║╚██╗██║██║  ██║ ' #  ╚════██║██║     ██╔══██╗██╔══╝  ██╔══╝  ██║╚██╗██║
+'███████╗██║ ╚████║██████╔╝ ' #  ███████║╚██████╗██║  ██║███████╗███████╗██║ ╚████║
+'╚══════╝╚═╝  ╚═══╝╚═════╝  ' #  ╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝
+                                                                                
+
 
 # end whileloop
 print("In generation",generation,"all giraffes are dead")
